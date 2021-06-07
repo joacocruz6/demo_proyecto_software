58e5fc834a6d096f95c04838f16e0a0c5dc8a0e7
Alejandra Alarcón <alalarco@dcc.uchile.cl>
2021-06-07 13:10:59 -0400

    feat(table): Show Afiliacion Bancaria table

--- src/components/afiliaciones/Afiliacion.jsx
+++ src/components/afiliaciones/Afiliacion.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Container, Table } from "react-bootstrap";
+import make_query from "../utils/make_query";
 
-const Afiliacion = () => {
-    return <div></div>;
+const Afiliacion = (props) => {
+  const [afilBanco, setBanco] = useState([]);
+  make_query(props.resource).then((jsonData) => {
+    const keys = Object.keys(jsonData.data.getAfiliacionBancaria);
+    const values = Object.values(jsonData.data.getAfiliacionBancaria);
+    let newArr = [];
+    for (let i = 0; i < keys.length; i++) {
+      newArr.push({ key: keys[i], value: values[i] });
+    }
+    setBanco(newArr);
+  });
+  
+  return (
+    <Container>
+      <h1>Afiliación Bancaria</h1>
+      <Table striped bordered responsive>
+        <thead>
+          <tr>
+            {afilBanco.map((el) => {
+              return <th>{el.key}</th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {afilBanco.map((el) => {
+              return <td>{el.value}</td>;
+            })}
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
+  );
 };
 
 export default Afiliacion;
